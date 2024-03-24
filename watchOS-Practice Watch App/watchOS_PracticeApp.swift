b2776ccfc9ff9f6c35154e14d91e576f7bffb1bd
Takasur Azeem <Takasur.Azeem1@ibm.com>
2024-03-24 23:26:58 +0500

    git init

---
+++ watchOS-Practice Watch App/watchOS_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOS_PracticeApp.swift
+//  watchOS-Practice Watch App
+//
+//  Created by Takasur Azeem on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Practice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchOS-Practice Watch AppUITests/watchOS_Practice_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  watchOS_Practice_Watch_AppUITestsLaunchTests.swift
+//  watchOS-Practice Watch AppUITests
+//
+//  Created by Takasur Azeem on 24/03/2024.
+//
+
+import XCTest
+
+final class watchOS_Practice_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
